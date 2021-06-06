68856155fb7940d9e03b813b99026b71a51659d1
Trankos <60846344+pabloskiii@users.noreply.github.com>
2021-06-06 19:43:34 +0200

    Update database.sql

--- db/database.sql
+++ db/database.sql
@@ -5,6 +5,8 @@ USE lamp_db;
 CREATE TABLE users (
   id int(11) NOT NULL auto_increment,
   name varchar(100) NOT NULL,
+  apellido1 varchar(100) NOT NULL,
+  apellido2 varchar(100) NOT NULL,
   age int(3) NOT NULL,
   email varchar(100) NOT NULL,
   PRIMARY KEY (id)
